8f592984904aa5f1310d1d0b33a2862a81ae8d7f
aus-0-tin <austin.waithaka@student.moringaschool.com>
2024-05-22 12:11:02 +0300

    completes lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
+def return_evens(numbers):
+    """Returns a list of even elements from the input list using list comprehension."""
+    return [num for num in numbers if num % 2 == 0]
 
 def return_evens(num_list):
     pass
+def make_exclamation(sentences):
+    """Returns a list of sentences with exclamation marks at the end using list comprehension."""
+    return [sentence + "!" for sentence in sentences]
 
 def make_exclamation(sentence_list):
     pass
